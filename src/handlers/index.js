2df33c256e6f9801c577646fe83ca74ea24ca10b
Saxon Landers <saxon@ackwell.com.au>
2018-09-12 22:46:15 +1000

    Default to Base when no handler is available

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,8 +1,9 @@
 import Action from './Action'
+import Base from './Base'
 
 const handlers = {
 	Action,
 }
 export default handlers
 
-export const getHandler = type => handlers[type]
+export const getHandler = type => handlers[type] || Base
