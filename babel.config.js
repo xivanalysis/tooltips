d535cdb7f5135bbe9dc71050b4401645c241436c
ackwell <saxon@ackwell.com.au>
2020-10-02 20:57:51 +1000

    Config for typescript + react

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+module.exports = api => ({
   presets: [
-    '@babel/preset-env'
+    '@babel/preset-env',
+    ['@babel/preset-typescript', {
+      allowDeclareFields: true,
+      onlyRemoveTypeImports: true,
+    }],
+    ['@babel/preset-react', {
+      development: api.env('development'),
+    }],
   ]
-}
+})
