a97cae59ed84f6bfdff1e52d7600f99005604c65
ackwell <saxon@ackwell.com.au>
2020-10-07 22:43:48 +1100

    Export label/detail

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
 import {Provider, ProviderProps} from './provider'
+import {Label, LabelProps} from './label'
+import {Detail, DetailProps} from './detail'
 
-export {Provider as TooltipProvider}
-export type {ProviderProps as TooltipProviderProps}
+export {
+	Provider as TooltipProvider,
+	Label as TooltipLabel,
+	Detail as TooltipDetail,
+}
+export type {
+	ProviderProps as TooltipProviderProps,
+	LabelProps as TooltipLabelProps,
+	DetailProps as TooltipDetailProps,
+}
